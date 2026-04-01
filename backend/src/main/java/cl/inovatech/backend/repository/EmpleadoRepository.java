5c8538242ffadb7bb780881e9bf608e7ba21b323
PAAWSA02LC1028 <Duoc@PAAWSA02LC1028.duoc.acad>
2026-04-01 16:41:26 -0300

    backend listo

---
+++ backend/src/main/java/cl/inovatech/backend/repository/EmpleadoRepository.java
@@ -0,0 +1,7 @@
+package cl.inovatech.backend.repository;
+
+import cl.inovatech.backend.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpleadoRepository extends JpaRepository<Empleado,Long>{
+}
